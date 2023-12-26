0d3ee58c351cc7c2b3f25cdf6eee1eb2b1e4ba64
Om Nigam <om2021064@akgec.ac.in>
2023-12-27 00:20:23 +0530

    designing contactus page

--- pages/contact.js
+++ pages/contact.js
@@ -1,12 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "@/styles/contact.module.css";
 
-const contact = () => {
+const Contact = () => {
+  // State variables to store form input values
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [description, setDescription] = useState("");
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = { name, email, phone, description };
+    // You can handle the form submission logic here, e.g., sending data to a server.
+    fetch("http://localhost:3000/api/postcontact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((resData) => {
+        console.log("Successfully sent contact informations");
+      })
+      .catch((err) => {
+        console.log("Error sending contact informations");
+      });
+
+    // Clear form fields after submission
+    setName("");
+    setEmail("");
+    setPhone("");
+    setDescription("");
+  };
+
   return (
-    <div>
-      <h1>Contact Us</h1>
+    <div className={styles.contactContainer}>
+      <h1 className={styles.heading}>Contact Us</h1>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div className={styles.formGroup}>
+          <label htmlFor="name" className={styles.label}>
+            Name:
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+            className={styles.inputs}
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="email" className={styles.label}>
+            Email:
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            className={styles.inputs}
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="phone" className={styles.label}>
+            Phone:
+          </label>
+          <input
+            type="number"
+            id="phone"
+            name="phone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            required
+            className={styles.inputs}
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="description" className={styles.label}>
+            Description:
+          </label>
+          <textarea
+            id="description"
+            name="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+            className={styles.textarea}
+          />
+        </div>
+        <button type="submit" className={styles.submitBtn}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
 
-export default contact;
+export default Contact;
